3cf2431bf43b986a066c3610c8c6f959a8686f2f
Mayank Jain <mayankjain1784@gmail.com>
2023-07-18 20:55:40 +0530

    header and footer setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Middleware to serve static files
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+
+app.listen(3000,(req,res) => {
+    console.log('Server is running on port 3000');
+});
