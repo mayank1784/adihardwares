02c615852114270593a34f1886110f684ac8fe23
Mayank Jain <mayankjain1784@gmail.com>
2023-08-04 21:36:19 +0530

    PORT updated for deployment

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use("/api/v1",user);
 app.use("/api/v1",product);
 
 
-app.listen(process.env.PORT, (req, res) => {
+app.listen(process.env.PORT || 3000, (req, res) => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 //Unhandled promise rejection
