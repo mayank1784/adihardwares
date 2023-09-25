9a9e9e5c53d4233748bb56c5ded30be80d9957de
Mayank Jain <67159533+mayank1784@users.noreply.github.com>
2023-09-25 22:23:17 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
+const connectDatabase = async() => {
+ try{ await mongoose
     .connect(process.env.DB_URL, 
         {
       useNewUrlParser: true,
